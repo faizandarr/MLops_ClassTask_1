8079d8125070f4dfb716e86a0cf2d5ca27918e94
Mohammmad Faizan <faizandar6420160@gmail.com>
2025-02-18 14:08:35 +0500

    basic

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return "Hello, Flask!"
 
+# Required for Vercel Deployment
+def handler(event, context):
+    return app(event, context)
+
 if __name__ == '__main__':
     app.run(debug=True)
