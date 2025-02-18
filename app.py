c731d4a0ac5055e126a9fbaf615e8113b2c18d68
Mohammmad Faizan <faizandar6420160@gmail.com>
2025-02-18 14:05:08 +0500

    basic

--- app.py
+++ app.py
@@ -1,33 +1,10 @@
-from flask import Flask, request, render_template, send_from_directory
-from werkzeug.utils import secure_filename
-import os
+from flask import Flask
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = 'uploads/'
-os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 
-@app.route('/', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        if 'file' not in request.files:
-            return render_template("index.html", error="No file part")
-        
-        file = request.files['file']
-        
-        if file.filename == '':
-            return render_template("index.html", error="No selected file")
-        
-        if file:
-            filename = secure_filename(file.filename)
-            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            file.save(filepath)
-            return render_template("index.html", filename=filename)
-    
-    return render_template("index.html")
-
-@app.route('/uploads/<filename>')
-def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+@app.route('/')
+def home():
+    return "Hello, Flask!"
 
 if __name__ == '__main__':
     app.run(debug=True)
