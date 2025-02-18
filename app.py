0cca231c26e833517fcab9994074bf025a14a7a5
Mohammmad Faizan <faizandar6420160@gmail.com>
2025-02-18 18:39:05 +0500

    Upgrading app

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return "This app is running perfectly" 
+    bmi = None
+    category = None
+    if request.method == 'POST':
+        weight = float(request.form['weight'])
+        height = float(request.form['height'])
+        
+        # Calculate BMI
+        bmi = round(weight / (height ** 2), 2)
+        
+        # Determine BMI category
+        if bmi < 18.5:
+            category = "Underweight"
+        elif 18.5 <= bmi < 24.9:
+            category = "Normal weight"
+        elif 25 <= bmi < 29.9:
+            category = "Overweight"
+        else:
+            category = "Obesity"
+    
+    return render_template('index.html', bmi=bmi, category=category)
 
 if __name__ == '__main__':
     app.run(debug=True)
